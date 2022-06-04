f8d591e48c19a7360c600c040b3cfa3213c83605
haruto.kubo <haruto20190525@gmail.com>
2022-06-04 05:17:20 +0000

    first commit

---
+++ js/slideshow.js
@@ -0,0 +1,34 @@
+$(window).on("load", function() {
+    $("#thumbnail img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#slideshow-image img").attr("src", img_src);
+    });
+});
+
+$(window).on("load", function() {
+    $("#thumbnail1 img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#slideshow-image1 img").attr("src", img_src);
+    });
+});
+
+$(window).on("load", function() {
+    $("#thumbnail2 img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#slideshow-image2 img").attr("src", img_src);
+    });
+});
+
+$(window).on("load", function() {
+    $("#thumbnail3 img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#slideshow-image3 img").attr("src", img_src);
+    });
+});
+
+$(window).on("load", function() {
+    $("#thumbnail4 img").click(function() {
+        let img_src = $(this).attr("src");
+        $("#slideshow-image4 img").attr("src", img_src);
+    });
+});
